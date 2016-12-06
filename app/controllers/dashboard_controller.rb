64d9a4a3f88644578fb7b4928f156233b73ee6cc
Blake Larson <blake7@hawaii.edu>
2016-12-05 15:32:07 -1000

    Add raw swl to users watched list.

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -5,6 +5,14 @@ class DashboardController < ApplicationController
   def profile
     # Convert to Raw SQL (double join, use join to get other tables)
     # Possibly add genre to table
-    @my_watched_movies = WatchedMovie.where(user_id: current_user.id)
+    @my_watched_movies = WatchedMovie.connection
+      .select_all("SELECT m.title AS title,
+                          w.created_at AS created_at
+                   FROM watched_movies w
+                   INNER JOIN movies m
+                   ON m.id = w.movie_id
+                   WHERE w.user_id = #{current_user.id};")
+
+
   end
 end
